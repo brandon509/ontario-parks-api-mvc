4f46c2a5a67aa9aa94f7be848722bd3e478601e5
Brandon Cousins <brandon509@gmail.com>
2022-12-12 21:14:37 -0500

    added parks object

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+let parks = {
+    "parkName": "Point Farms",
+    "location": "Goderich",
+    "region": "Southerm",
+    "phoneNumber": "519-543-9432"
+}
+
 app.get('/', (req,res) => {
-    res.json('hello world')
+    res.json(parks)
 })
 
 const PORT = process.env.PORT || 8000
