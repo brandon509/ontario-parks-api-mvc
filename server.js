22b5d4a2d259ff60f6f74693ca5bfb1c6a677a0b
Brandon Cousins <brandon509@gmail.com>
2022-12-19 21:40:38 -0500

    set up mongodb and created put route to add parks to db

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 const cors = require('cors')
 const morgan = require('morgan')
 const fs = require('fs')
 const path = require('path')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 
 let parks = [
@@ -24,41 +26,73 @@ let parks = [
         "yearEstablished": "1971",
         "phoneNumber": "705-789-5105"
     }
-    ]   
+    ]
+
+const connString = process.env.CONNECTION_STRING
+
+MongoClient.connect(connString)
+    .then(client => {
+        const db = client.db('ontario-provincial-parks')
+        const dbCollection = db.collection('ontario-provincial-parks')
+        const logFile = fs.createWriteStream(path.join(__dirname, "server.log"), {flags: 'a'})
+
+        //======================================
+        // Middlewares
+        //======================================
+
+        app.use(express.json())
+        app.use(morgan('combined', {stream: logFile}))
+        app.use(cors())
+
+        //======================================
+        // Routes
+        //======================================
+
+        app.get('/api/parks/all', (req,res) => {
+            res.json(parks)
+        })
+
+        app.get('/api/parks/region/:region',(req,res) => {
+            const region = req.params.region.toLowerCase()
+            const oneRegion = parks.filter(x => x.region.toLowerCase() == region)
+            if(oneRegion.length > 0){
+                res.json(oneRegion)   
+            }
+            else{
+                res.status(404).json({error: 'region does not exist'})
+            }
+        })
+
+        app.get('/api/parks/name/:park',(req,res) => {
+            const parkName = req.params.park.toLowerCase()
+            const parkInfo = parks.find(x => x.name.toLowerCase() == parkName)
+            if(parkInfo){
+                res.json(parkInfo)
+            }
+            else{
+                res.status(404).json({error: 'park does not exist'})
+            }
+        })
+
+        app.post('/api/new', (req,res) => {
+            dbCollection.insertOne(req.body)
+            .then(result => {
+                console.log(`${req.body.name} has been added to the db...`)
+                res.json(`${req.body.name} has been added to the db...`)
+            })
+            .catch(err => console.error(err))
+        })
 
 
-const logFile = fs.createWriteStream(path.join(__dirname, "server.log"), {flags: 'a'})
-app.use(morgan('combined', {stream: logFile}))
-app.use(cors())
+        //======================================
+        // Listener
+        //======================================
+
+        const PORT = process.env.PORT || 8000
+        app.listen(PORT, () => {
+            console.log(`server running on port ${PORT} `)
+        })
 
 
-app.get('/api/parks/all', (req,res) => {
-    res.json(parks)
-})
-
-app.get('/api/parks/region/:region',(req,res) => {
-    const region = req.params.region.toLowerCase()
-    const oneRegion = parks.filter(x => x.region.toLowerCase() == region)
-    if(oneRegion.length > 0){
-        res.json(oneRegion)   
-    }
-    else{
-        res.status(404).json({error: 'region does not exist'})
-    }
-})
-
-app.get('/api/parks/name/:park',(req,res) => {
-    const parkName = req.params.park.toLowerCase()
-    const parkInfo = parks.find(x => x.name.toLowerCase() == parkName)
-    if(parkInfo){
-        res.json(parkInfo)
-    }
-    else{
-        res.status(404).json({error: 'park does not exist'})
-    }
-})
-
-const PORT = process.env.PORT || 8000
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT} `)
-})
+    })
+    .catch(err => console.error(err))
