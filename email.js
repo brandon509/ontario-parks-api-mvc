c8b45bad67f9b53d2ed05541f0e5de6ea81a1d43
Brandon Cousins <brandon509@gmail.com>
2023-01-03 22:41:54 -0500

    added email functionality

---
+++ email.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer')
+
+async function main(){
+    try{
+        let testAccount = await nodemailer.createTestAccount()
+        let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        post: 587,
+        secure: false,
+        auth: {
+            user: testAccount.user,
+            pass: testAccount.pass
+        }
+    })
+
+    let info = await transporter.sendMail({
+        from: 'foo@example.com',
+        to: 'boo@example.com',
+        subject: 'Open this email for your token' ,
+        text: `Here is your token:` ,
+        html: "<h1>Did my test work</h1>"
+    })
+
+    console.log('Message Sent: %s', info.messageId)
+
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+    }
+   
+    catch(err){
+        console.log(error)
+    }
+}
+
+
+
+module.exports = main
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const data = require('./data.js')
 const auth = require('./authentication')
+const email = require('./email')
 
 const app = express()
 let cache = apicache.middleware
@@ -33,6 +34,16 @@ async function main(){
     //======================================
     // Routes
     //======================================
+
+    app.get('/api/test', async (req,res) => {
+        try{
+            email()
+            res.json('Worked')
+        }
+        catch(err){
+            res.json('Fail')
+        }
+    })
 
     app.get('/api/parks/all', auth, async (req,res) =>{
         try{
@@ -197,91 +208,6 @@ async function main(){
 //make so users cant have true
 //email token to general user
 //route to make admins true
-
-// MongoClient.connect(connString)
-//     .then(client => {
-//         console.log('database connected successfully...')
-//         const db = client.db('ontario-provincial-parks')
-//         const dbCollection = db.collection('ontario-provincial-parks')
-//         const logFile = fs.createWriteStream(path.join(__dirname, "server.log"), {flags: 'a'})
-
-//         //======================================
-//         // Middlewares
-//         //======================================
-
-//         app.use(express.json())
-//         app.use(morgan('combined', {stream: logFile}))
-//         app.use(cors())
-//         app.use(cache('5 minutes'))
-
-        //======================================
-        // Routes
-        //======================================
-
-//         app.get('/api/parks/all', (req,res) => {
-//             dbCollection.find().toArray()
-//             .then(result => {
-//                 res.json(result)
-//             })
-//             .catch(err => console.error(err))
-//         })
-
-//         app.get('/api/parks/region/:region',(req,res) => {
-//             const region = req.params.region.toLowerCase()
-//             dbCollection.find({region: `${region}`}).toArray()
-//                 .then(result => {
-//                     if(result.length != 0){
-//                         res.json(result) 
-//                     }
-//                     else{
-//                         res.status(404).json({error: `region ${region} does not exist`}) 
-//                     }
-//                 })
-//                 .catch(err => console.error(err))
-//         })
-
-//         app.get('/api/parks/name/:park',(req,res) => {
-//             const parkName = req.params.park.toLowerCase()
-//             dbCollection.findOne({name: `${parkName}`})
-//                 .then(result => {
-//                     if(result){
-//                         res.json(result) 
-//                     }
-//                     else{
-//                         res.status(404).json({error: `park ${parkName} does not exist`}) 
-//                     }
-//                 })
-//                 .catch(err => console.error(err))
-//         })
-
-//         app.post('/api/new', (req,res) => {
-//             dbCollection.insertOne(req.body)
-//             .then(result => {
-//                 console.log(`${req.body.name} has been added to the db...`)
-//                 res.json(`${req.body.name} has been added to the db...`)
-//             })
-//             .catch(err => console.error(err))
-//         })
-
-
-//         //======================================
-//         // Listener
-//         //======================================
-
-//         const PORT = process.env.PORT || 8000
-//         app.listen(PORT, () => {
-//             console.log(`server running on port ${PORT}...`)
-//         })
-
-
-//     })
-//     .catch(err => {
-//         console.log('database connection failed, exiting now...')
-//         console.error(err)
-//         process.exit(1)
-//     })
-
-
 
     //next steps
     //add in all data
